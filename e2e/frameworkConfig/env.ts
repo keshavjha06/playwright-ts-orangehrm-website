c7eb99046a48192ca344277a3df7835c6d3c2275
Keshav <keshav@forge.gg>
2024-08-20 09:43:26 +0530

    prettier config

--- e2e/frameworkConfig/env.ts
+++ e2e/frameworkConfig/env.ts
@@ -1,5 +1,5 @@
 export class Env {
-    public static readonly BASE_URL = process.env.URL ?? "Default";
-    public static readonly USERNAME = process.env.USERNAME ?? "Default";
-    public static readonly PASSWORD = process.env.PASSWORD ?? "Default"
+  public static readonly BASE_URL = process.env.URL ?? "Default";
+  public static readonly USERNAME = process.env.USERNAME ?? "Default";
+  public static readonly PASSWORD = process.env.PASSWORD ?? "Default";
 }
