d1096516e41e26980ce0209662da8bb0e9d4cd3f
Keshav <keshav@forge.gg>
2026-05-01 21:01:58 +0530

    Add compiled JavaScript files from TypeScript upgrade

---
+++ e2e/frameworkConfig/env.js
@@ -0,0 +1,5 @@
+export class Env {
+    static BASE_URL = process.env.URL ?? "Default";
+    static USERNAME = process.env.USERNAME ?? "Default";
+    static PASSWORD = process.env.PASSWORD ?? "Default";
+}
---
+++ e2e/pages/components/topMenuComponent.js
@@ -0,0 +1,11 @@
+class TopMenuComponent {
+    page;
+    constructor(page) {
+        this.page = page;
+    }
+    topMenu = (menuName) => this.page.getByRole("link", { name: menuName });
+    async selectTopMenuItem(menuItem) {
+        await this.topMenu(menuItem).click();
+    }
+}
+export default TopMenuComponent;
