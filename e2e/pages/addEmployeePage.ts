14483263f39824ce45fcbb085160c340be743584
Keshav <keshav@forge.gg>
2026-05-01 21:01:24 +0530

    Fix TypeScript initialization errors after package upgrade

--- e2e/pages/addEmployeePage.ts
+++ e2e/pages/addEmployeePage.ts
@@ -2,25 +2,35 @@ import { Page } from "@playwright/test";
 import { EmployeeDetails } from "../testdata/orangeHrmInterfaces";
 
 class AddEmployeePage {
-  constructor(private readonly page: Page) {}
+  constructor(private readonly page: Page) { }
 
-  private readonly firstNameTextBox = this.page.getByRole("textbox", {
-    name: "First Name",
-  });
-  private readonly lastNameTextBox = this.page.getByRole("textbox", {
-    name: "Last Name",
-  });
-  private readonly middleNameTextBox = this.page.getByRole("textbox", {
-    name: "Middle Name",
-  });
-  private readonly idTextBox = this.page
-    .locator("form")
-    .getByRole("textbox")
-    .nth(4);
-  private readonly saveButton = this.page.getByRole("button", { name: "Save" });
-  public readonly successMessage = this.page.getByText(/Successfully Saved/i);
+  private declare firstNameTextBox: ReturnType<Page["getByRole"]>;
+  private declare lastNameTextBox: ReturnType<Page["getByRole"]>;
+  private declare middleNameTextBox: ReturnType<Page["getByRole"]>;
+  private declare idTextBox: ReturnType<Page["locator"]>;
+  private declare saveButton: ReturnType<Page["getByRole"]>;
+  public declare successMessage: ReturnType<Page["getByText"]>;
+
+  async initialize() {
+    this.firstNameTextBox = this.page.getByRole("textbox", {
+      name: "First Name",
+    });
+    this.lastNameTextBox = this.page.getByRole("textbox", {
+      name: "Last Name",
+    });
+    this.middleNameTextBox = this.page.getByRole("textbox", {
+      name: "Middle Name",
+    });
+    this.idTextBox = this.page
+      .locator("form")
+      .getByRole("textbox")
+      .nth(4);
+    this.saveButton = this.page.getByRole("button", { name: "Save" });
+    this.successMessage = this.page.getByText(/Successfully Saved/i);
+  }
 
   async addEmployee(employeeDetails: EmployeeDetails) {
+    await this.initialize();
     await this.firstNameTextBox.fill(employeeDetails.firstName);
     await this.lastNameTextBox.fill(employeeDetails.lastName);
     await this.middleNameTextBox.fill(employeeDetails.middleName);
