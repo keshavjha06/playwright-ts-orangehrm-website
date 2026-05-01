dce2fe013a042c326bef15e42131d4033b5acefd
Keshav <keshav@forge.gg>
2026-05-01 22:48:10 +0530

    Refactor: format API test requests and clean up tsconfig paths

--- e2e/tests/api/reqres.spec.js
+++ e2e/tests/api/reqres.spec.js
@@ -1,7 +1,9 @@
 import { test, expect } from "@playwright/test";
 
 test("GET /users", async ({ request }) => {
-  const response = await request.get("https://jsonplaceholder.typicode.com/users");
+  const response = await request.get(
+    "https://jsonplaceholder.typicode.com/users",
+  );
   expect(response.status()).toBe(200);
   const body = await response.json();
   expect(body[0].id).toBe(1);
